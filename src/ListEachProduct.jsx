756551f6db66bc5e4cbc41792ce10396fde7e9b8
chandan0507 <chandanubale8@gmail.com>
2025-04-12 09:57:33 +0530

    message

--- src/ListEachProduct.jsx
+++ src/ListEachProduct.jsx
@@ -44,7 +44,7 @@ export default function ListEachProduct({ eachSingleProduct, product }) {
                 backgroundImage: `url(${eachSingleProduct['thumbnail']})`,
                 backgroundSize: 'cover'
             }}></div>
-            <div className="grid grid-cols-3 gap-3">
+            <div className="grid grid-cols-3 gap-1">
 
                 <div className="text-white col-span-3 text-xl">{eachSingleProduct?.title}</div>
                 <div className={`text-white my-2 rounded-full ${getColor} px-2 w-16 py-1`}>{eachSingleProduct?.rating} ☆</div>
